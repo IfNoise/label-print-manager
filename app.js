e27dc629ac52adc8f52abbd9f61003c1a321b96c
IfNoise <black_russan@mail.ru>
2024-01-17 16:22:37 +0700

    изменено:      app.js 	изменено:      config/production.json

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express=require("express")
 const config=require("config")
+const https = require('https');
+const fs = require('fs');
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const cors=require("cors")
@@ -8,6 +10,21 @@ const path=require('path')
 const PORT=config.get("port")||5000
 
 const app=express()
+
+let options
+if(process.env.NODE_ENV==='development'){
+options = {
+
+  key: fs.readFileSync(__dirname + '/ssl/homeserver.key', 'utf8'),
+ cert: fs.readFileSync(__dirname + '/ssl/homeserver.crt', 'utf8')
+};}else{
+  options = {
+
+    key: fs.readFileSync(__dirname + '/ssl/hangar2.key', 'utf8'),
+   cert: fs.readFileSync(__dirname + '/ssl/hangar2.crt', 'utf8')
+}}
+const server=https.createServer(options,app)
+
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json({extended:true}))
@@ -19,7 +36,7 @@ async function start(){
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
-    app.listen(PORT,()=>console.log(`Server has been started at ${PORT}`)
+    server.listen(PORT,()=>console.log(`Server has been started at ${PORT}`)
     )
   } catch (error) {
     console.log('SErver error',error.message)
