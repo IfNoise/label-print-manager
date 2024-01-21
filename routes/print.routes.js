1640dcdc09dbebd264790aea49ac1b3213db7dbf
IfNoise <black_russan@mail.ru>
2024-01-21 22:56:39 +0700

    изменено:      .gitignore 	изменено:      label.pdf 	изменено:      routes/print.routes.js

--- routes/print.routes.js
+++ routes/print.routes.js
@@ -3,11 +3,11 @@ const TrayItem = require("../models/TrayItem");
 const Plant = require("../models/Plant");
 const router = Router();
 const fs = require("fs");
-const { Image,createCanvas } = require('canvas')
+const { Image, createCanvas } = require("canvas");
 const cups = require("node-cups");
 const QRCode = require("qrcode");
 
-const printPlants=async (plants)=>{
+const printPlants = async (plants) => {
   try {
     const tray = await Promise.all(
       plants.map(async (id) => {
@@ -28,39 +28,44 @@ const printPlants=async (plants)=>{
         return result;
       })
     );
-    await Promise.all(tray.map(async(plant)=>{
-      const id=plant.id.toString()
-      const qrCodeImagePath ="./qr/"+ id+".png";
-      QRCode.toFile(qrCodeImagePath,id,{width:75,height:75,margin:2});
-    }) )
-    const myPDFcanvas = createCanvas(142, 85, 'pdf')
-    const ctx = myPDFcanvas.getContext('2d')
-      tray.forEach((plant) => {
-        const id=plant.id.toString()
-        const qrCodeImagePath = './qr/'+id+".png";
-        const img = new Image()
-        img.src = qrCodeImagePath
-        ctx.drawImage(img, 65,0,75,75)
-        ctx.font = 'bold 18px Arial '
-        ctx.fillText(plant.pheno, 3, 17,64)
-        ctx.font = ' 12px Arial '
-        ctx.fillText(plant.strain, 3, 28,52)
-        ctx.font = '8px Arial '
-        ctx.fillText(plant.type, 3, 36,52)
-        ctx.font = '10px Arial '
-        ctx.fillText('start:'+plant.start, 3, 49,62)
-        ctx.addPage(142,85)
-        fs.rm(qrCodeImagePath,(err)=>{
-          console.log(err)
-          
-        })
-  })
-      const buff = myPDFcanvas.toBuffer('application/pdf')
-      fs.writeFile('label.pdf', buff, function (err) {
-        if (err) throw err
-      
-        console.log('created label.pdf')
+    await Promise.all(
+      tray.map(async (plant) => {
+        const id = plant.id.toString();
+        const qrCodeImagePath = "./qr/" + id + ".png";
+        QRCode.toFile(qrCodeImagePath, id, {
+          width: 75,
+          height: 75,
+          margin: 2,
+        });
       })
+    );
+    const myPDFcanvas = createCanvas(142, 85, "pdf");
+    const ctx = myPDFcanvas.getContext("2d");
+    tray.forEach((plant) => {
+      const id = plant.id.toString();
+      const qrCodeImagePath = "./qr/" + id + ".png";
+      const img = new Image();
+      img.src = qrCodeImagePath;
+      ctx.drawImage(img, 66, 5, 75, 75);
+      ctx.font = "bold 22px Arial ";
+      ctx.fillText(plant.pheno, 3, 20, 64);
+      ctx.font = " 12px Arial ";
+      ctx.fillText(plant.strain, 3, 30 ,52);
+      ctx.font = "8px Arial ";
+      ctx.fillText(plant.type, 3, 40, 52);
+      ctx.font = "10px Arial ";
+      ctx.fillText("start:" + plant.start, 3, 55, 62);
+      ctx.addPage(142, 85);
+      fs.rm(qrCodeImagePath, (err) => {
+        console.log(err);
+      });
+    });
+    const buff = myPDFcanvas.toBuffer("application/pdf");
+    fs.writeFile("label.pdf", buff, function (err) {
+      if (err) throw err;
+
+      console.log("created label.pdf");
+    });
     const printerNames = await cups.getPrinterNames();
     console.log(printerNames);
     const options = {
@@ -75,42 +80,37 @@ const printPlants=async (plants)=>{
       } else {
         console.log(`Этикетка успешно отправлена на печать. Job ID: ${jobID}`);
         res.send("Этикетка успешно отправлена на печать.");
-        
       }
     });
-    return  tray 
+    return tray;
   } catch (error) {
-    return error
+    return error;
   }
-}
-
+};
 
 router.post("/print_tray", async (req, res) => {
   try {
     const data = await TrayItem.find({}, "plantId").exec();
-    const plants=data.map((plant)=>(plant.plantId))
-    const result= await printPlants(plants)
-    
+    const plants = data.map((plant) => plant.plantId);
+    const result = await printPlants(plants);
+
     res.json({ result });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 });
 router.post("/print_plants", async (req, res) => {
-  const plants=req.body.plants
-  if(!plants){
+  const plants = req.body.plants;
+  if (!plants) {
     return res.status(500).json({ message: "Nothing for printing" });
   }
   try {
-    
-    const result= await printPlants(plants)
-    
+    const result = await printPlants(plants);
+
     res.json({ result });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 });
-
-
 
 module.exports = router;
